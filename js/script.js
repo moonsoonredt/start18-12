c38cf2bb54bb08f01b311d82222f7f5286c5602d
moonsoonredt <romchik898@ya.ru>
2023-12-20 11:50:10 +0300

    Раскомментировал Удалил цикл

--- js/script.js
+++ js/script.js
@@ -19,15 +19,11 @@ let personalMovieBD = {
 }
 
 // Задать пользователю по 2 раза вопросы
-for (let i = 0; i < 2; i++) {
-    let questFilm = prompt("Один из последних просмотренных фильмов?");
-    let questFilmRet = prompt("На сколько оцените его?");
-    personalMovieBD.movies[questFilm] = questFilmRet;
-}
-// let questLastFilm1 = prompt("Один из последних просмотренных фильмов?");
-// let questLastFilmReyt = prompt("На сколько оцените его?");
-// personalMovieBD.movies[questLastFilm1] = questLastFilmReyt;
-// let questLastFilm3 = prompt("Один из последних просмотренных фильмов?");
-// let questLastReyt2 = prompt("На сколько оцените его?");
-// personalMovieBD.movies[questLastFilm3] = questLastReyt2;
-// console.log(personalMovieBD)
+
+let questLastFilm1 = prompt("Один из последних просмотренных фильмов?");
+let questLastFilmReyt = prompt("На сколько оцените его?");
+personalMovieBD.movies[questLastFilm1] = questLastFilmReyt;
+let questLastFilm3 = prompt("Один из последних просмотренных фильмов?");
+let questLastReyt2 = prompt("На сколько оцените его?");
+personalMovieBD.movies[questLastFilm3] = questLastReyt2;
+console.log(personalMovieBD)
