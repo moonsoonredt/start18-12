e8b90a1cfff716fe9e9d0ed8055f5c8081172896
moonsoonredt <romchik898@ya.ru>
2024-02-20 16:19:06 +0300

    фиксация в вс код

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 'use strict'
 
+const btn = document.querySelectorAll('button');
+const overlay = document.querySelector('.overlay');
+// btn.onclick = function() {
+//     alert('World')
+// }
 
+// btn.addEventListener('click', () => {
+//     alert('World');
+// })
 
-// Удалить блок разметки содержащиеся в классе "promo__adv"
-let promo = document.querySelector('.promo__adv');
-promo.remove();
+// btn.addEventListener('click', () => {
+//     alert('World2');
+// })
+
+// let i = 0;
+const deleteElement = (e) => {
+  console.log(e.target);
+  console.log(e.type);
+//   i++;
+//   if (i == 3) {
+//     btn.removeEventListener("click", deleteElement);
+//   }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btn.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once:true});
+})
+
+const link = document.querySelector('a');
+link.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(event.target);
+})
